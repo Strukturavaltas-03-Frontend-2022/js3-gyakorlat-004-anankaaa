6dcf4be9bc7fc1413c664363dff1fcfa3addca66
Anankaaa <hegyaljai83@hotmail.com>
2022-09-12 15:30:16 +0200

    done

--- 11-regex/exercises/01/solution/js/validateVisaCardNumber.js
+++ 11-regex/exercises/01/solution/js/validateVisaCardNumber.js
@@ -1,1 +1,5 @@
+const validateVisaCardNumber = (visaCard) => {
+  const visaRegExp = /^4([0-9]{12}|[0-9]{15})$/;
+  return visaRegExp.test(visaCard);
+};
 export default validateVisaCardNumber;
--- 11-regex/exercises/03/solution/js/validateMACAddress.js
+++ 11-regex/exercises/03/solution/js/validateMACAddress.js
@@ -1,1 +1,6 @@
+const validateMACAddress = (mac) => {
+    const macAddress = /^([0-9A-Fa-f]{2}[:-]){5}([0-9A-Fa-f]{2})$/;
+    return macAddress.test(mac);
+}
+
 export default validateMACAddress;
